c8a68f79520632ca56a003b2effb7c0ec05f3f07
Hack Reactor Student <student@hackreactor.com>
2017-11-03 15:45:33 -0700

    Create three tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,24 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  username TEXT
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INTEGER PRIMARY KEY,
+  rooms_id INTEGER,
+  users_id INTEGER,
+  message_text TEXT,
+  FOREIGN KEY(rooms_id) REFERENCES rooms(id),
+  FOREIGN KEY(users_id) REFERENCES users(id)
 );
 
 /* Create other tables and define schemas for them here! */
